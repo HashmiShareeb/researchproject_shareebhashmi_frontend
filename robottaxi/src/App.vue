c6d3b8ea20be786f9c282a8ddf3ec6fb4691c7cd
shareeb hashmi <shareeb.hashmi@student.howest.be>
2025-01-14 20:27:18 +0100

    add Tailwind CSS setup, create DashboardScreen and AppHeader components, and configure Vue Router

--- robottaxi/src/App.vue
+++ robottaxi/src/App.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="dark:bg-gray-800 flex flex-col bg-white min-h-screen">
+    <div class="maw-w-screen-xl mx-auto">
+      <AppHeader />
+      <RouterView />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
+<!-- <div
+class="container bg-gradient-to-br from-blue-500 to-purple-500 h-screen flex justify-center items-center"
+>
+<h1 class="text-4xl text-white text-center">welcome</h1>
+</div> -->
---
+++ robottaxi/src/router/router.ts
@@ -0,0 +1,22 @@
+import {
+  createRouter,
+  createWebHistory,
+  type Router,
+  type RouteRecordRaw,
+} from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Home",
+
+    component: () => import("../views/DashboardScreen.vue"),
+  },
+];
+
+const router: Router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
